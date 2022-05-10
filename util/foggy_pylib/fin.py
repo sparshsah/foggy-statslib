66fe897a7814672e9882ceed73db156150a395bc
Sparsh Sah <sparshsah@gmail.com>
2022-05-09 22:50:42 -0400

    comma

--- util/foggy_pylib/fin.py
+++ util/foggy_pylib/fin.py
@@ -15,7 +15,7 @@ Notes
     specified market param based on the given data sample.
 ** Currently, each estimator adheres to a frequentist paradigm,
      using the ex-post sample stat directly as its point estimate of the parameter.
-     But, in the future, we could implement a more Bayesian approach,
+     But in the future, we could implement a more Bayesian approach,
      using the data to instead inform our posterior distribution for the parameter.
 """
 
