adcaadbdc52284f7a66cf21edf3a80146c1ba7c9
Sparsh Sah <sparshsah@gmail.com>
2022-06-21 02:03:25 -0700

    skeleton

---
+++ util/foggy_pylib/__init__.py
@@ -0,0 +1,13 @@
+"""Python utitilies package.
+
+author: [@sparshsah](https://github.com/sparshsah)
+
+
+# Structure
+* core
+* stat(istic)s
+    - sim(ulations)
+    - est(imation)
+    - t(ime)s(eries) a(nalysis)
+* fin(ance)
+"""
---
+++ util/foggy_pylib/stats/__init__.py
@@ -0,0 +1,10 @@
+"""Statistics module.
+
+author: [@sparshsah](https://github.com/sparshsah)
+
+
+# Structure
+* sim(ulations)
+* est(imation)
+* t(ime)s(eries) a(nalysis)
+"""
