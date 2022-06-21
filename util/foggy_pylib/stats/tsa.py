7cb03dec73707d69f05c3171f90a6c606c6d93d9
Sparsh Sah <sparshsah@gmail.com>
2022-06-21 03:18:34 -0700

    crap

--- util/foggy_pylib/stats/tsa.py
+++ util/foggy_pylib/stats/tsa.py
@@ -9,7 +9,7 @@ author: [@sparshsah](https://github.com/sparshsah)
 from typing import Union, Optional
 import pandas as pd
 # https://github.com/sparshsah/foggy-lib/blob/main/util/foggy_pylib/core.py
-import foggy_pylib.core as fc
+import foggy_pylib.core as fc  # type: ignore
 
 FloatSeries = pd.Series
 FloatDF = pd.DataFrame
