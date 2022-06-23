461c5d1b6b9f3805ed6f5581e7ede6100ea4ca10
Sparsh Sah <sparshsah@gmail.com>
2022-06-23 14:50:29 -0700

    amean

--- util/foggy_pylib/stats/est/__init__.py
+++ util/foggy_pylib/stats/est/__init__.py
@@ -1,9 +1,11 @@
-# some preprocessing
+# https://en.wikipedia.org/wiki/Generalized_mean
 import pandas as pd
 from foggy_pylib.stats.est.core import get_qmean, get_gmean, get_hmean
 # fill them in if necessary
 if not hasattr(pd.Series, "qmean"):
     pd.Series.qmean = get_qmean
+if not hasattr(pd.Series, "amean"):
+    pd.Series.amean = pd.Series.mean
 if not hasattr(pd.Series, "gmean"):
     pd.Series.gmean = get_gmean
 if not hasattr(pd.Series, "hmean"):
