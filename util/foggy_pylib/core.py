b0a9525626b75fc6871c95a3caf849dc8dc1973d
Sparsh Sah <sparshsah@gmail.com>
2022-06-23 23:21:40 -0700

    up a tick

--- util/foggy_pylib/core.py
+++ util/foggy_pylib/core.py
@@ -1,6 +1,6 @@
 """Catch-all utility module.
 
-v1.0 beta: API probably won't dramatically change, but
+v1.1 beta: API probably won't dramatically change, but
     implementations have not yet been thoroughly tested.
 
 author: [@sparshsah](https://github.com/sparshsah)
