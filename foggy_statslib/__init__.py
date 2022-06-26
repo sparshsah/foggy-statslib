130aecd8aec0980220df7eed1437fddf203d675b
Sparsh Sah <sparshsah@gmail.com>
2022-06-25 18:40:55 -0700

    put this in the readme

--- foggy_statslib/__init__.py
+++ foggy_statslib/__init__.py
@@ -1,13 +1,4 @@
 """Python utitilies package.
 
 author: [@sparshsah](https://github.com/sparshsah)
-
-
-# Structure
-* core
-* stat(istic)s
-    - sim(ulations)
-    - est(imation)
-    - t(ime)s(eries) a(nalysis)
-* fin(ance)
 """
